773d2f65d8c9064530884a912f9ea1a85f54f746
Marcos Rodrigues <marcos.rodrigues@miniclip.com>
2019-01-03 11:18:12 +0000

    Create hero prefab

---
+++ Assets/HeroMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour
+{
+    Vector2 destination;
+    float range;
+    bool isMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isMoving)
+        {
+            //draw circle and arrow
+        }
+    }
+
+    private void OnMouseDown() 
+    {
+        isMoving = true;
+    }
+
+    private void OnMouseUp() 
+    {
+        if(isMoving)
+        {
+            isMoving = false;
+            destination = 
+        }
+    }
+}
