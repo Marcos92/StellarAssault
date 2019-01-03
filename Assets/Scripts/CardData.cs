b2335008fa9aa61c08fc5efc997258aac9b1de69
manuel.barbosa <manuel.barbosa@miniclip.com>
2019-01-03 15:08:11 +0000

    Add Card Content Logic
    Add Card Content Logic

---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardData : ScriptableObject
+{
+    public int Cooldown = 0;
+    
+    public Sprite Image = null;
+    
+    public string Title = "Game Card Title";
+
+    public string Description = "This is the card Description";
+
+    public GameObject Hero = null;
+    
+}
