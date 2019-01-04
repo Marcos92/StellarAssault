ea1157f8f9401ebd4605fd991213b53078dd43c8
João Seixas <joao.seixas@miniclip.com>
2019-01-04 14:22:30 +0000

    Cherry pick

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -1,20 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
+    // Enums
+    public enum AttackType
+    {
+        CircleAttack,
+        ConeAttack,
+        DonnutAttack,
+        LineAttack
+    };
+
+    // Characteristics
+    public float maxMovementRange;
+    public float attackRange;
+    public Color Color;
+    public AttackType attackType;
+
+    // Movement
     Vector2 destination;
     Vector2 endTurnPosition;
-    public float maxRange;
-    bool isPressed;
+    Vector2 direction;
     LineRenderer line;
     GameObject arrow;
     GameObject finalPosition;
-    All_Seeing_Eye allSeingEye;
     bool newDestination = false;
+    bool isPressed;
+    bool isBlocked;
 
-    public Color Color;
+    // Attack
+    bool attackOnce = true;
+    GameObject attackAnimation;
+
+    // Manager
+    All_Seeing_Eye allSeingEye;
 
     // Start is called before the first frame update
     void Start()
@@ -27,11 +49,16 @@ public class Hero : MonoBehaviour
         line.endColor = Color;
 
         arrow = transform.Find("Arrow").gameObject;
+        attackAnimation = transform.Find("Attack_Anim").gameObject;
+
         finalPosition = transform.Find("Final Position").gameObject;
         allSeingEye = GameObject.Find("Illuminatti").gameObject.GetComponent<All_Seeing_Eye>();
+
+        //Callback registration
         allSeingEye.RegisterTurnEndCallback(HandleOnTurnEndCallbackDelegate);
         allSeingEye.RegisterMovementCallback(HandleMovementCallbackDelegate);
         allSeingEye.RegisterActionCallback(HandleActionCallbackDelegate);
+        allSeingEye.RegisterActionEndCallback(HandleActionEndCallbackDelegate);
     }
 
     // Update is called once per frame
@@ -55,10 +82,10 @@ public class Hero : MonoBehaviour
 
             Vector2 direction = currentMousePos - transformPositionV2;
 
-            if (direction.sqrMagnitude > maxRange * maxRange)
+            if (direction.sqrMagnitude > maxMovementRange * maxMovementRange)
             {
                 direction.Normalize();
-                currentMousePos = transformPositionV2 + (direction * maxRange);
+                currentMousePos = transformPositionV2 + (direction * maxMovementRange);
             }
 
             line.SetPosition(1, currentMousePos);
@@ -80,6 +107,20 @@ public class Hero : MonoBehaviour
     public void ClearHighlight() {
         transform.GetComponent<SpriteRenderer>().color = Color.white;
     }
+<<<<<<< HEAD
+=======
+    
+    private void FixedUpdate() {
+        if(!isPressed)
+            return;
+
+        LayerMask mask = LayerMask.GetMask("Obstacle");
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, maxMovementRange, mask);
+        isBlocked = hit.collider != null && hit.collider.transform != transform;
+        Debug.DrawRay(transform.position, hit.point, Color.green);
+        Debug.Log(hit.collider.transform.gameObject.name);
+    }
+>>>>>>> 7c05bf4... Add attack animation
 
     private void OnMouseDown() 
     {
@@ -132,7 +173,46 @@ public class Hero : MonoBehaviour
     void HandleActionCallbackDelegate()
     {
         // Already moved, so we can reset newDestination
-        newDestination = false;
+        if (newDestination)
+        {
+            newDestination = false;
+        }
+
+        if (attackOnce)
+        {
+            attackOnce = false;
+            AnimateAttack();
+            ApplyAttackOnArea();
+        }
+    }
+
+    private void ApplyAttackOnArea()
+    {
+        switch(attackType)
+        {
+            case AttackType.CircleAttack:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void HandleActionEndCallbackDelegate()
+    {
+        attackOnce = true;
+        Animator attackAnimationAnimator = attackAnimation.GetComponent<Animator>();
+        attackAnimationAnimator.StopPlayback();
+        attackAnimation.SetActive(false);
+    }
+
+
+    void AnimateAttack()
+    {
+        attackAnimation.transform.position = transform.position;
+        attackAnimation.SetActive(true);
+
+        Animator attackAnimationAnimator = attackAnimation.GetComponent<Animator>();
+        attackAnimationAnimator.Play("Attac", -1, 0.0f);
     }
 
 }
